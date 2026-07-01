278e4658cf68fcda183c345063537d98f0904799
aliyasalim971 <aliya.salim@experionglobal.com>
2026-07-01 11:43:57 +0530

    Create athena_queries.sql
    Added Athena SQL queries

---
+++ sql/athena_queries.sql
@@ -0,0 +1,18 @@
+-- View sales summary
+SELECT *
+FROM sales_summary;
+
+-- Customer revenue
+SELECT *
+FROM customer_revenue
+ORDER BY customer_revenue DESC;
+
+-- Product performance
+SELECT *
+FROM product_performance
+ORDER BY product_sales DESC;
+
+-- Region revenue
+SELECT *
+FROM region_sales
+ORDER BY region_revenue DESC;
